8815f183ff7c10957b3cb4c54eee6e59349e6762
dizline <52178204+dizline@users.noreply.github.com>
2020-05-31 11:33:09 +0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "@dimaspnr4", quotes: "Whatever happens, keep breathing" },
+    { author: "@dimaspnr4", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "@dimaspnr4", quotes: "Hey! I luv youuu! UwU " },
+    { author: "@dimaspnr4", quotes: "I love you." },
+    { author: "@dimaspnr4", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "@dimaspnr4", quotes: "I love you." },
+    { author: "@dimaspnr4", quotes: "I'm sorry for loving you." },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Mine❣️", "Caca", "Sayang", "Val<3"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
