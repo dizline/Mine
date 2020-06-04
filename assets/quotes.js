780d14a497aa657893ee28cb4ea11161f429ff3a
dizline <52178204+dizline@users.noreply.github.com>
2020-06-04 18:25:23 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "@dimaspnr4", quotes: "Whatever happens, keep breathing" },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️" },
     { author: "@dimaspnr4", quotes: "Ily Kania ❣️" },
-    { author: "@dimaspnr4", quotes: "Hey! I luv youuu! UwU " },
-    { author: "@dimaspnr4", quotes: "I love you." },
-    { author: "@dimaspnr4", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "@dimaspnr4", quotes: "I love you." },
-    { author: "@dimaspnr4", quotes: "I'm sorry for loving you." },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️" },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️." },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️." },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️." },
+    { author: "@dimaspnr4", quotes: "Happy Birthday Kania ❣️." },
 ];
