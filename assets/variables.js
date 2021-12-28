75db38a7940c05ad19e3b1837539e6bf9a07f184
Dimas Pnr <52178204+dizline@users.noreply.github.com>
2021-12-28 15:23:20 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -2,7 +2,7 @@ export default {
   nicknames: ["byy", "byy", "byy", "okeii"],
   greetings: {
     evening: "have a nice dream",
-    afternoon: "Istirahat yang cukup okeii",
+    afternoon: "Istirahat yang cukup byy",
     day: "Pagii!",
     morning: "jangan lupa sarapan..",
     night: "good night and sweet dreams"
